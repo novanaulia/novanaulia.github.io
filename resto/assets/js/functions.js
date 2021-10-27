637063508092bccfee96cc5abebaa024d55b1139
novanaulia <novan.aulia@gmail.com>
2021-10-27 11:57:47 +0700

    coba commit 1

---
+++ resto/assets/js/functions.js
@@ -0,0 +1,92 @@
+/* ALERT */
+//alert("Hello, world!");
+
+let nama_depan = "novan";
+let nama_belakang = "aulia";
+
+let nama_lengkap = `${nama_depan} ${nama_belakang}`;
+
+//alert(nama_lengkap);
+
+let barang = 80;
+let jumlah = 2;
+let total = barang * jumlah;
+
+//alert("Total Barang " + total);
+
+/* KONSTANTA */
+const negara = "indonesia";
+const database = "jwp";
+
+console.log("Total barang via konsol = " + total);
+
+//document.write(total);
+
+/* KONDISIONAL */
+/*  
+let nama = prompt("Siapa Namamu?", "");
+
+if (nama == "novan") {
+  alert("Benar kamu novan, boleh masuk");
+} else {
+  alert("kamu gak boleh masuk");
+}
+*/
+
+/* LOOPING */
+/*
+for (let i = 0; i <= 10; i++) {
+  document.write(i + "<br>");
+}
+
+let i = 100;
+while (i <= 120) {
+  document.write(i + "<br>");
+  i++;
+}
+*/
+
+/* FUNCTION */
+
+/*
+function greeting() {
+  if (nama_depan == "novan") {
+    document.write("benar nama kamu " + nama_depan);
+  } else {
+    document.write("salah ini bukan kamu");
+  }
+}
+greeting();
+
+*/
+
+function login(username, password) {
+  if (username && password) {
+    document.write("Selamat datang user Anda " + username + " dengan password " + password);
+    //document.write("Selamat datang Anda sudah masuk");
+  } else if (username || password) {
+    document.write("user password salah");
+  } else {
+    document.write("belum login.");
+  }
+}
+
+let username = "rendang";
+let password = "teh";
+
+login(username, password);
+
+function luas_segitiga(a, t) {
+  luas = (1 / 2) * (a, t);
+  document.write("<p>" + luas + "</p>");
+}
+
+let a, t;
+a = 10;
+t = 20;
+luas_segitiga(a, t);
+
+/* STRUKTUR DATA */
+
+let fruits = ["Apple", "Orange", "Plum"];
+document.write(fruits[2]);
